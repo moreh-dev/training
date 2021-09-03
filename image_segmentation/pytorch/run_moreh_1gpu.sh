1808ba10389c4160d708218ce3c4b61a174fef33
ChangseokSong <Changseok.Song@moreh.io>
2021-09-03 23:49:10 +0900

    add scripts

---
+++ image_segmentation/pytorch/run_moreh_1gpu.sh
@@ -0,0 +1,23 @@
+DATESTAMP=${DATESTAMP:-`date +'%y%m%d%H%M%S%N'`}
+BENCHMARK=${BENCHMARK:-"unet_3d"}
+BENCHMARK_NAME="unet3d"
+LOGDIR=${LOGDIR:-"./results/$BENCHMARK"}
+LOGFILE_BASE="${LOGDIR}/${DATESTAMP}"
+GPU=${GPU:-1}
+mkdir -p $(dirname "${LOGFILE_BASE}")
+touch "./results/unet3d.log"
+
+python main.py \
+    --data_dir /home/share/dataset/mlperf/3d-unet/data \
+    --epochs 4000 \
+    --evaluate_every 20 \
+    --start_eval_at 1000 \
+    --quality_threshold 0.908 \
+    --batch_size 2 \
+    --optimizer sgd \
+    --ga_steps 1 \
+    --learning_rate 0.8 \
+    --seed 0 \
+    --lr_warmup_epochs 200 \
+    -v \
+    --log_dir $LOGDIR |& tee ${LOGFILE_BASE}_moreh_1gpu.log
---
+++ image_segmentation/pytorch/run_nvidia_google.sh
@@ -0,0 +1,21 @@
+DATESTAMP=${DATESTAMP:-`date +'%y%m%d%H%M%S%N'`}
+BENCHMARK=${BENCHMARK:-"unet_3d"}
+BENCHMARK_NAME="unet3d"
+LOGDIR=${LOGDIR:-"./results/$BENCHMARK"}
+LOGFILE_BASE="${LOGDIR}/${DATESTAMP}"
+GPU=${GPU:-1}
+mkdir -p $(dirname "${LOGFILE_BASE}")
+touch "./results/unet3d.log"
+
+python -m torch.distributed.launch --nproc_per_node=$GPU --master_port=15921 main.py \
+    --data_dir /home/share/dataset/mlperf/3d-unet/data \
+    --epochs 10000 \
+    --evaluate_every 20 \
+    --start_eval_at 1000 \
+    --quality_threshold 0.908 \
+    --batch_size 4 \
+    --optimizer sgd \
+    --ga_steps 1 \
+    --learning_rate 4 \
+    --lr_warmup_epochs 1000 \
+    --log_dir $LOGDIR |& tee ${LOGFILE_BASE}_nvidia_google.log
